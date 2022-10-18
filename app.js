07466491bceef29a1edcb7578183634ec7a9514c
techzgigz <gigz.techz@gmail.com>
2022-10-18 20:55:53 +0530

    api

--- app.js
+++ app.js
@@ -1,61 +1,27 @@
-const createError = require("http-errors");
 const express = require("express");
-const cors = require("cors");
-const cookieParser = require("cookie-parser");
-const logger = require("morgan");
-const winston = require("winston");
-
-const { authorizeUser } = require("@app/util/authorize");
-
 const app = express();
-
-app.use(logger("combined"));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 
-const corsOptions = {
-  origin: "*",
-};
+// const corsOptions = {
+//   origin: "*",
+// };
 
-app.use(cors(corsOptions));
+// app.use(cors(corsOptions));
 
 app.use(function (req, res, next) {
-  winston.info("API : " + req.originalUrl);
-  winston.info("Header : " + JSON.stringify(req.headers, null, 4));
-  winston.info("Request : " + JSON.stringify(req.body, null, 4));
-  let oldSend = res.send;
-  res.send = function (data) {
-    oldSend.apply(res, arguments);
-    winston.info("Response : " + JSON.stringify(data, null, 4));
-  };
   next();
 });
 
-app.use(authorizeUser);
+// app.use(authorizeUser);
 
-app.use("/api/user", require("@app/routes/user"));
-app.use("/api/video", require("@app/routes/video"));
-app.use("/api/comment", require("@app/routes/comment"));
-app.use("/api/watchedVideo", require("@app/routes/watched_video"));
-app.use("/api/admin", require("@app/routes/admin"));
+ 
 
 // catch 404 and forward to error handler
-app.use(function (req, res, next) {
-  next(createError(404));
-});
+// app.use(function (req, res, next) {
+//   next(createError(404));
+// });
 
-const errorLogger = (error, req, res, next) => {
-  // for logging errors
-  winston.error(error); // or using any fancy logging library
-  next(error); // forward to next middleware
-};
-
-const failSafeHandler = (err, _, res, __) =>
-  res
-    .status(err.status || 500)
-    .json({ status: err.status, message: err.message });
-
-app.use(errorLogger);
-app.use(failSafeHandler);
-
-module.exports = app;
+ 
+app.listen(4000);
+// module.exports = app;
--- controllers/users/validate.js
+++ controllers/users/validate.js
@@ -1,43 +1,7 @@
-const Joi = require("joi");
-const schemas = {
-  userAdd: Joi.object().keys({
-    phone: Joi.string().max(10).required(),
-    firebaseUid: Joi.string().required(),
-    fcmToken: Joi.string().required(),
-    firstName: Joi.string().required(),
-    surName: Joi.string().required(),
-    userName: Joi.string().min(4).max(10).required(),
-    gender: Joi.string().valid("Male", "Female", "Others").required(),
-    city: Joi.string().required(),
-    state: Joi.string().required(),
-    status: Joi.string().valid("Active", "Suspended").required(),
-  }),
-  userLogin: Joi.object().keys({
-    phone: Joi.string().max(10).required(),
-    fcmToken: Joi.string().required(),
-  }),
-  userNameExists: Joi.object().keys({
-    userName: Joi.string().min(4).max(10).required(),
-  }),
-  updateVideoCategory: Joi.object().keys({
-    videoCategory: Joi.array().required(),
-  }),
-  updateUserStatus: Joi.object().keys({
-    _id: Joi.string().required(),
-    status: Joi.string().valid("Active", "Suspended").required(),
-  }),
-  followUnfollow: Joi.object().keys({
-    followeeUserId: Joi.string().required(),
-  }),
-  favUnfav: Joi.object().keys({
-    videoId: Joi.string().required(),
-  }),
-  getUserById: Joi.object().keys({
-    _id: Joi.string().required(),
-  }),
-  adminLogin: Joi.object().keys({
-    userName: Joi.string().required(),
-    phone: Joi.string().max(10).required(),
-  }),
-};
-module.exports = schemas;
+// const Joi = require("joi");
+// const schemas = {
+//   userAdd: Joi.object().keys({
+//   }),
+   
+// };
+// module.exports = schemas;
