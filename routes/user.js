29f75d5b291c0b03d169748f436c2dfda80d5e20
techzgigz <gigz.techz@gmail.com>
2022-10-18 22:42:10 +0530

    res.send(data);

--- routes/user.js
+++ routes/user.js
@@ -4,4 +4,5 @@ const userController = require("../controllers/users/resolvers");
 
 
 router.get("/",   userController.saveUser); 
+router.post("/",   userController.saveUser); 
 module.exports = router;
